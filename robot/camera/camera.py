45906f6abf318a239c334ff5f45f41883a847f02
Liibert Paul <oliibert@hotmail.com>
2019-09-23 19:26:31 +0300

    e

--- robot/camera/camera.py
+++ robot/camera/camera.py
@@ -1,6 +1,7 @@
 import pickle
 import struct
 import cv2
+import imutils
 import numpy as np
 import pyrealsense2 as rs
 
@@ -19,6 +20,15 @@ class Camera:
         color_frame_1 = frames_1.get_color_frame()
         color_image_1 = np.asanyarray(color_frame_1.get_data())
         _, frame = cv2.imencode('.jpg', color_image_1)
-        data = pickle.dumps(frame, 0)
-        size = len(data) s
+
+        frame = imutils.resize(frame, width=600)
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+        mask = cv2.inRange(hsv, np.array([69, 91, 41]), np.array([95, 255, 255]))
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+
+        data = pickle.dumps(mask, 0)
+        size = len(data)
         return struct.pack(">L", size) + data
